70e25c1a5fa77d7af83ad389dea7e9e7c3b81713
d2shikha <deepshikhadohare@gmail.com>
2020-01-28 18:15:26 +0530

    backend

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded());
+app.use(express.static('public'));
+
+app.get('/',(req , res)=>{
+	res.sendFile(__dirname + '/index.html');
+});
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port , ()=>console.log(`running on &{port}`));
